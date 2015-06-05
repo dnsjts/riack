149f412f370dff2527245614d0f45e658c2845f2
htrap <htrapdev@gmail.com>
2015-06-05 18:19:20 +0530

    adding tests for riack_client_send

--- tests/check_client.c
+++ tests/check_client.c
@@ -46,6 +46,70 @@ START_TEST (test_riack_client_connect_and_disconnect)
 
   riack_client_free (client);
 }
+
+
+START_TEST (test_riack_client_send)
+{
+  riack_client_t *client;
+  riack_put_req_t *putreq;
+  riack_content_t *content;
+
+
+  client = riack_client_new ();
+
+  
+
+  if (network_tests_enabled ())
+    {
+      /* Connect with default options! */
+      
+      ck_assert_errno (riack_client_connect
+                       (client,
+                        RIACK_CONNECT_OPTION_HOST, "localhost",
+                        RIACK_CONNECT_OPTION_NONE), 0);
+                        
+                        
+      
+      content = riack_content_new ();
+      putreq = riack_req_put_new ();
+
+      ck_assert_errno
+           (riack_content_set (content,
+                        RIACK_CONTENT_FIELD_VALUE, "some-value", -1,
+                        RIACK_CONTENT_FIELD_CONTENT_TYPE, "text/plain", -1,
+                        RIACK_CONTENT_FIELD_CONTENT_ENCODING, "none", -1,
+                        RIACK_CONTENT_FIELD_CHARSET, "utf8", -1,
+                        RIACK_CONTENT_FIELD_NONE),
+     0);
+     
+      ck_assert_errno
+            (riack_req_put_set (putreq,
+                         RIACK_REQ_PUT_FIELD_BUCKET, "new-bucket",
+                         RIACK_REQ_PUT_FIELD_BUCKET_TYPE, "set",
+                         RIACK_REQ_PUT_FIELD_KEY, "030620151900",
+                         RIACK_REQ_PUT_FIELD_CONTENT, content,
+                         RIACK_REQ_PUT_FIELD_NONE),
+                         0);
+
+  
+      ck_assert_errno
+            (riack_client_send (client,
+                                RIACK_MESSAGE_PUTREQ,
+                                putreq),
+                                0);
+                                
+ 
+
+  
+  riack_req_put_free (putreq);
+  
+      riack_client_disconnect (client);
+
+      
+    }
+
+  riack_client_free (client);
+}
 END_TEST
 
 static TCase *
@@ -56,6 +120,7 @@ test_riack_client (void)
   test_client = tcase_create ("Client");
   tcase_add_test (test_client, test_riack_client_new_and_free);
   tcase_add_test (test_client, test_riack_client_connect_and_disconnect);
+  tcase_add_test (test_client, test_riack_client_send);
 
   return test_client;
 }
